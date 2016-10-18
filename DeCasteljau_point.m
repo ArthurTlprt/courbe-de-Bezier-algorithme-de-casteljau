e2117cfe722a992e60cac52d9015d7dd27e0280a
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-10-18 12:38:30 +0200

    1.a) fonctionne

--- DeCasteljau_point.m
+++ DeCasteljau_point.m
@@ -1,16 +1,4 @@
+Px = [1 2 5 8 9];
+Py = [2 6 7 6 2];
 
-Pts = [1, 2; 2, 6; 5, 7; 8, 6; 9, 2];
-
-ptsToKeep = zeros(size(Pts));
-ptsToKeep(1, 1:2) = Pts(1, 1:2)
-
-% pts(1, end+1) = (Px(1)+Px(2))/2;
-% pts(2, end+1) = (Py(1)+Py(2))/2;
-
-
-% for i=1:length(Px)-1
-%   pts(end+1) = [(Px(i)+Px(i+1))/2, (Py(i)+Py(i+1))/2];
-% end
-
-% pts = zeros(2, length(Px)*2-1)
-% interp_decasteljau(Px, Py, []);
+interp_decasteljau(Px, Py, 0.5)
--- interp_decasteljau.m
+++ interp_decasteljau.m
@@ -1,9 +1,18 @@
-function pts = interp_decasteljau(previousPts, ptsToKeep)
-  if length(previousPts) == 0
-    pts = ptsTokeep;
+function pt = interp_decasteljau(controlex, controley, t)
+  n = length(controlex);
+  Px = [];
+  Py = [];
+  for i=1:n
+    Px(1, i) = controlex(i);
+    Py(1, i) = controley(i);
   end
-  for i=1:length(Px)-1
-    pts(end+1) = [(Px(i)+Px(i))/2, (Py(i)+Py(i))/2];
+  for j=2:n+1
+    for i=1:n-j+1
+      Px(j, i) = (1-t)*Px(j-1, i) + t*Px(j-1, i+1);
+      Py(j, i) = (1-t)*Py(j-1, i) + t*Py(j-1, i+1);
+    end
   end
-  pts = interp_decasteljau(previousPts, ptsToKeep)
+  Px
+  Py
+  pt = [Px(n,1), Py(n,1)];
 end
