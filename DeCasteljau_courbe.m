ce6627d5c0a077f4f0eeda61968312f2bf0ee8cd
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-10-18 13:03:11 +0200

    1.b) fait

---
+++ DeCasteljau_courbe.m
@@ -0,0 +1,17 @@
+Px = [1 2 5 8 9];
+Py = [2 6 7 6 2];
+
+echantillons = 0:0.1:1;
+Pix = [];
+Piy = [];
+for i=1:length(echantillons)
+  Pix(i) = interp_decasteljau(Px, Py, echantillons(i))(1)
+  Piy(i) = interp_decasteljau(Px, Py, echantillons(i))(2)
+end
+Pix
+Piy
+
+Pn(1, 1:length(Pix)) = Pix;
+Pn(2, 1:length(Piy)) = Piy;
+
+tracer_courbe_bezier(transpose(Pn), length(Pix)-1);
